42f79394352dd46756efd00485b616d9781ea007
NIKHIL PATEL <115173157+nikhilpatel9@users.noreply.github.com>
2026-06-11 15:33:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,21 @@ function mincost(arr)
 { 
 //write your code here
 // return the min cost
-  
+	let ans=0;
+	while(arr.length==1){
+		const minValue1 = Math.min(...arr);
+	    const minIndex1 = arr.indexOf(minValue1);
+	    arr.splice(minIndex1, 1);
+			const minValue2 = Math.min(...arr);
+		    const minIndex2 = arr.indexOf(minValue2);
+		    arr.splice(minIndex2, 1);
+		let temp=(minValue1+minValue2);
+		arr.push(temp);
+		ans+=temp;
+		
+	}
+	
+  return ans;
 }
 
 module.exports=mincost;
